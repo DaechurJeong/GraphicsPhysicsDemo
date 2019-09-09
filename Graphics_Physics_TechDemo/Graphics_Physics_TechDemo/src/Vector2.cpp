35af9d74537efacab874681c18c6f06bd363986b
nahye.park <nahye.park@digipen.edu>
2019-09-09 03:17:38 -0700

    Math & Physics
    implement vector2 (math)

---
+++ Graphics_Physics_TechDemo/Graphics_Physics_TechDemo/src/Vector2.cpp
@@ -0,0 +1,99 @@
+#include "Vector2.h"
+#include <math.h>
+
+
+void vector2::operator=(const vector2& _rhs)
+{
+	x = _rhs.x;
+	y = _rhs.y;
+}
+
+void vector2::operator+=(const vector2& _rhs)
+{
+	x += _rhs.x;
+	y += _rhs.y;
+}
+
+void vector2::operator-=(const vector2& _rhs)
+{
+	x -= _rhs.x;
+	y -= _rhs.y;
+}
+
+void vector2::operator+=(const float& val)
+{
+	x += val;
+	y += val;
+}
+
+void vector2::operator-=(const float& val)
+{
+	x -= val;
+	y -= val;
+}
+
+void vector2::operator*=(const float& val)
+{
+	x *= val;
+	y *= val;
+}
+
+void vector2::operator/=(const float& val)
+{
+	x /= val;
+	y /= val;
+}
+
+float vector2::getDistance()
+{
+	return sqrtf(x * x + y * y);
+}
+
+float vector2::getSquaredDistance()
+{
+	return x * x + y * y;
+}
+
+vector2 vector2::operator+(const vector2& _rhs)
+{
+	vector2 result = *this;
+	result += _rhs;
+	return result;
+}
+
+vector2 vector2::operator-(const vector2& _rhs)
+{
+	vector2 result = *this;
+	result -= _rhs;
+	return result;
+}
+
+vector2 vector2::operator+(const float& _rhs)
+{
+	vector2 result = *this;
+	result.x += _rhs;
+	result.y += _rhs;
+	return result;
+}
+
+vector2 vector2::operator-(const float& _rhs)
+{
+	vector2 result = *this;
+	result.x -= _rhs;
+	result.y -= _rhs;
+	return result;
+}
+
+vector2 vector2::operator-()
+{
+	return vector2(-x, -y);
+}
+
+
+vector2 vector2::dot(const vector2& _rhs)
+{
+	vector2 result = *this;
+	result.x *= _rhs.x;
+	result.y *= _rhs.y;
+	return result;
+}
---
+++ Graphics_Physics_TechDemo/Graphics_Physics_TechDemo/src/Vector2.h
@@ -0,0 +1,42 @@
+#pragma once
+
+
+
+struct vector2 {
+	float getX() { return x; }
+	float getY() { return y; }
+	void setX(float _x) { x = _x; }
+	void setY(float _y) { y = _y; }
+	float getDistance();
+	float getSquaredDistance();
+	vector2 dot(const vector2& _rhs);
+
+	//constructor
+	vector2() = default;
+	vector2(float _x, float _y) { x = _x; y = _y; }
+	vector2(const vector2& _rhs) { *this = _rhs; }
+
+	//operator
+	
+	vector2 operator+(const vector2& _rhs);
+	vector2 operator-(const vector2& _rhs);
+	vector2 operator+(const float& _rhs);
+	vector2 operator-(const float& _rhs);
+	vector2 operator-();
+	void operator=(const vector2& _rhs);
+	void operator+=(const vector2& _rhs);
+	void operator-=(const vector2& _rhs);
+	void operator+=(const float& val);
+	void operator-=(const float& val);
+	void operator*=(const float& val);
+	void operator/=(const float& val);
+
+
+
+
+
+
+	//data
+	float x;
+	float y;
+};
